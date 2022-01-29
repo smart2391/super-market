c64baf4c76d96dc1c4da07306e4435474e20acd2
smart2391 <abdullayevdilshodbek20@gmail.com>
2022-01-29 14:56:20 +0500

    edit ApiController

---
+++ src/main/java/uz/smart/market/controller/ApiController.java
@@ -0,0 +1,16 @@
+package uz.smart.market.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+    @GetMapping("/hello")
+    public String method(){return "hello world"; }
+
+    @GetMapping("/git")
+    public String git(){
+
+        return "Hello git";
+    }
+}
